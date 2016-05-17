c4c871ed487dd8241a8cc25626f60d4b123593f4
daniel <daniel@localhost>
2016-05-17 23:29:18 +0200

    added Element() test

---
+++ tests/test_element.py
@@ -0,0 +1,34 @@
+
+import unittest
+from jobpip3.element import Element
+
+
+RECORD_AMOUNT = 3
+
+
+class Test1(Element):
+
+    def __init__(self, *args, **kwargs):
+        super(Test1, self).__init__(*args, **kwargs)
+        self.InRecord = None
+
+    def worker(self, records):
+        for i in xrange(RECORD_AMOUNT):
+            yield { 'i' : i, 'foo' : "bar" }
+
+
+
+class TestElement(unittest.TestCase):
+    """Element() tests"""
+
+    def test_output(self):
+
+        e = Test1()
+
+        i = 0
+        for r in e.flow():
+            self.assertEqual(r['i'], i)
+            self.assertEqual(r['foo'], "bar")
+            i += 1
+
+        self.assertEqual(i, RECORD_AMOUNT)
