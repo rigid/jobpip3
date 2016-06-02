224eb4d21ad9449b31c806d171c04b634328c2aa
daniel <daniel@localhost>
2016-06-02 22:49:17 +0200

    removed status() method and counters (moved to Element() class)

--- jobpip3/_function.py
+++ jobpip3/_function.py
@@ -1,5 +1,4 @@
 
-from panoracle.util import log
 from _element import Element
 
 
@@ -10,15 +9,6 @@ class Function(Element):
 
     def __init__(self, *args, **kwargs):
         super(Function, self).__init__(*args, **kwargs)
-
-        # total amount of records seen by this processor (passed + dismissed)
-        self.total = 0
-        # amount of records that passed through this processor
-        self.passed = 0
-        # amount of records that were not passed on
-        self.dismissed = 0
-        # amount of records that were touched by this processor
-        self.modified = 0
 
 
     def worker(self, records):
@@ -34,22 +24,3 @@ class Function(Element):
                 self.__class__.__name__
             )
         )
-
-
-    def status(self):
-        """print current state of this processor"""
-
-        # calculate missing values
-        if self.total == 0 and (self.passed != 0 or self.dismissed != 0):
-            self.total = self.passed + self.dismissed
-        elif self.passed == 0 and (self.total != 0 or self.dismissed != 0):
-            self.passed = self.total - self.dismissed
-        elif self.dismissed == 0 and (self.total != 0 or self.passed != 0):
-            self.dismissed = self.total - self.passed
-
-        log.info("{0}: passed: {2}, dismissed: {3}, "
-                 "modified: {4}, total: {1}".format(
-                self.__class__.__name__,
-                self.total, self.passed, self.dismissed,
-                self.modified
-            ))
