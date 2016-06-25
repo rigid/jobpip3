652895f96825413185fcf9c3429afe551a08fdca
daniel <daniel@localhost>
2016-06-26 00:17:48 +0200

    support record_class argument as string; pass serializable arguments to super(); increment self.passed

--- jobpip3/source_readfile.py
+++ jobpip3/source_readfile.py
@@ -1,5 +1,7 @@
 """a source that reads records from one or more files"""
 
+import os
+import importlib
 import types
 import itertools
 
@@ -11,22 +13,35 @@ from record import Record
 class Readfile(Source):
     """read records from file(s)"""
 
-    def __init__(self, files, record_class=Record, **kwargs):
-        """:param files: fd or list of fd objects
+    def __init__(self, files, record_class="Record", **kwargs):
+        """:param files: filename or list of filenames
            :param record_class: class of records that will
                 be read (default: Record)"""
-        super(Readfile, self).__init__(**kwargs)
+
+        if isinstance(record_class, (str, unicode)):
+            package = os.path.split(os.path.dirname(__file__))[-1]
+            record_module = importlib.import_module(package + ".record")
+            record_class = getattr(record_module, record_class)
+
+        super(Readfile, self).__init__(
+            files=files,
+            record_class=record_class.__name__,
+            **kwargs
+        )
 
         # handle arguments
-        self.files = files
-        if not isinstance(self.files, (list, types.GeneratorType)): self.files = [ self.files ]
+        self.filenames = files
+        if not isinstance(self.filenames, (list, types.GeneratorType)):
+            self.filenames = [ self.filenames ]
+
         self.OutRecord = record_class
 
 
     def well(self):
 
-        for fd in self.files:
-            for record in self.OutRecord.read(fd):
-                # output record
-                yield record
-
+        for file in self.filenames:
+            with open(file, 'rb') as fd:
+                for record in self.OutRecord.read(fd):
+                    # output record
+                    yield record
+                    self.passed += 1
