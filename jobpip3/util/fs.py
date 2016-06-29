62ef9c7f25157e6ffe34b332f4d4372c3424da84
daniel <daniel@localhost>
2016-06-30 00:27:33 +0200

    add path to dir() results

---
+++ jobpip3/util/fs.py
@@ -0,0 +1,43 @@
+"""filesystem utils"""
+
+import os
+import errno
+
+
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as e:  # Python >2.5
+        if e.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+
+def dir(destpath, prefix=None, suffix=None):
+
+    for filename in os.listdir(destpath):
+
+        # respect prefix ?
+        if prefix is not None:
+            # skip files that don't start with prefix
+            if not filename.startswith(prefix):
+                continue
+
+        # respect suffix ?
+        if suffix is not None:
+            # skip files that don't end with suffix of sink that is
+            # used by the aggregator (e.g. "*.json")
+            if not filename.endswith(suffix):
+                continue
+
+        yield destpath + '/' + filename
+
+
+def opendir(destpath, prefix=None, suffix=None):
+
+    for filename in dir(destpath, prefix=prefix, suffix=suffix):
+        # read records from file
+        yield open(destpath + "/" + filename, "rb")
+
