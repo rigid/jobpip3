ccb6911f5f01a7825d7182f5fb344b261bd7c396
daniel <daniel@localhost>
2016-08-21 11:42:59 +0200

    remove self.In/OutRecord logic; introduce self.has_in/output; remove internal keys before sinking

--- jobpip3/sinks/_sink.py
+++ jobpip3/sinks/_sink.py
@@ -7,15 +7,26 @@ class Sink(Element):
        drains them somewhere."""
 
     def __init__(self, *args, **kwargs):
-        super(Sink, self).__init__(*args, **kwargs)
+        # a sink receives records
+        self.has_input = True
         # a sink doesn't produce output records
-        self.OutRecord = None
+        self.has_output = False
+
+        super(Sink, self).__init__(*args, **kwargs)
+
+    @staticmethod
+    def _del_internal(records):
+        """remove internal attributes before passing record to sink"""
+        for r in records:
+            for k in r.keys():
+                if k.startswith("__") and k.endswith("__"): del r[k]
+            yield r
 
 
     def worker(self, records):
         """actual worker method"""
-        self.drain(records)
-        raise StopIteration()
+        self.drain(self._del_internal(records))
+        return []
 
 
     def drain(self, record):
